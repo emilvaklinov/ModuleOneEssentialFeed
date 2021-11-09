f9157da0c5eed5c7e19aacb2c449e6c4cd2cb573
Emil Vaklinov <emil_vaklinov@yahoo.com>
2021-11-09 16:36:45 +0000

    Replace optional params with success/failure cases

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -6,9 +6,15 @@
 //
 
 import Foundation
+import QuartzCore
+
+public enum HTTPClientResult {
+    case success(HTTPURLResponse)
+    case failure(Error)
+}
 
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping(Error?, HTTPURLResponse?) -> Void)
+    func get(from url: URL, completion: @escaping(HTTPClientResult) -> Void)
     
 }
 
@@ -27,11 +33,12 @@ public final class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping(Error) -> Void) {
-        client.get(from: url) { error, response in
-            if response != nil {
+        client.get(from: url) { result in
+            switch result {
+            case .success:
                 completion(.invalidData)
-            } else {
-            completion(.connectivity)
+            case .failure:
+                completion(.connectivity)
             }
         }
     }
